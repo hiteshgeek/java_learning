4030ebb2c072c7836e0e27b0845a7ded8daf1e6a
Hitesh Vaghela <hbvaghela83@gmail.com>
2025-02-07 12:42:31 +0530

    add methods

---
+++ Methods.java
@@ -0,0 +1,12 @@
+class Methods{
+    public void printString(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String args[]){
+        //call PrintString
+        Methods methods = new Methods();
+        methods.printString();
+    }
+}
+
